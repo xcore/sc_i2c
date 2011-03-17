0427a3c6649c364f84997fbed3523eb4a1c939ac
GopalaKrishna <gopalakrishna_nl@mindtree.com>
2011-03-17 17:10:36 +0530

    Changed The API Names and added new parameters

--- module_i2c/src/i2c.h
+++ module_i2c/src/i2c.h
@@ -13,24 +13,28 @@
 #define _i2c_master_h_
 
 #include <xs1.h>
-
+#include <platform.h>
 // I2C clock frequency is fref/I2C_BIT_TIME
 // where fref defaults to 100MHz
 #ifndef I2C_BIT_TIME
 #define I2C_BIT_TIME 1000
 #endif
 
+
+
 struct r_i2c {
-  out port scl;
-  port sda;
+	port scl;
+	port sda;
 };
+
 
 struct i2c_data_info {
 	unsigned int data[100];
 	unsigned int data_len;
 };
 
-int i2c_rd(int addr, int device, struct i2c_data_info &i2c_data,struct r_i2c &i2c);
-int i2c_wr(int addr, int device, struct i2c_data_info &i2c_data, struct r_i2c &i2c);
+int i2c_master_rd(int addr, int device, struct i2c_data_info &i2c_data,struct r_i2c &i2c_master);
+int i2c_master_wr(int addr, int device, struct i2c_data_info &i2c_data, struct r_i2c &i2c_master);
+int i2c_slave_rd(int dev_addr, struct i2c_data_info &i2c_data_slave, struct r_i2c &i2c_slave,out port st_det);
 
 #endif
