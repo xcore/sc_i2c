f6e3026ce4127f08b557672fd39a46d43eb0367a
GopalaKrishna <gopalakrishna_nl@mindtree.com>
2011-04-05 14:10:36 +0530

    Changed Code to include clock sync logic

--- module_i2c/src/i2c.h
+++ module_i2c/src/i2c.h
@@ -17,8 +17,8 @@
 // I2C clock frequency is fref/I2C_BIT_TIME
 // where fref defaults to 100MHz
 #ifndef I2C_BIT_TIME
-#define I2C_BIT_TIME 1000
-//#define I2C_MASTER_TX
+#define I2C_BIT_TIME 250
+#define I2C_MASTER_TX
 #endif
 
 
@@ -33,12 +33,13 @@ struct i2c_data_info {
 	unsigned int data[100];
 	unsigned int data_len;
 	unsigned int master_num;
+	unsigned int clock_mul;
 };
 
 int i2c_master_rx(int device, int sub_addr, struct i2c_data_info &i2c_data,struct r_i2c &i2c_master);
 int i2c_master_tx(int device, int sub_addr, struct i2c_data_info &i2c_data, struct r_i2c &i2c_master);
 int i2c_slave_rx(int dev_addr, struct i2c_data_info &i2c_data_slave, struct r_i2c &i2c_slave,out port st_det);
 int i2c_slave_tx(int dev_addr, int sub_addr, struct i2c_data_info &i2c_slave_data, struct r_i2c &i2c_slave,out port st_det);
-void wait_func(int div_factor);
+void wait_func(int div_factor, unsigned int clock_mul,port scl,int edge);
 
 #endif
